e35c691f441517ee2ba0a323f3a162fd85ad61f5
anas12312 <anashesham000@gmail.com>
2024-04-08 15:13:57 +0200

    anas

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 import { UserMessage } from './Message';
 import './style.css'
 
+let userMessage = null;
+
 const chatEl = document.getElementById('chat');
+const inputEl = document.getElementById('input-area')
+const sendButtonEl = document.getElementById('send-button')
+
+const handleInpur = () => {
+    userMessage = inputEl.textContent.trim()
+    if(!userMessage) return;
+    inputEl.textContent = ""
+    chatbox.appendChild(generateMessage(userMessage, "user"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    
+    setTimeout(() => {
+        const incomingChatLi = generateMessage("Thinking...", "bot");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        // generateResponse(incomingChatLi);
+    }, 600);
+}
+
 
 window.addEventListener('keypress', (e) => {
     if(e.key === 'a') {
